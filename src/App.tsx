5a71814326e1a7182f8c59843e530e2be4611bc9
Alan Sartorio <alan42ga@hotmail.com>
2024-06-01 22:29:05 -0300

    Update react, add post creation form

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,24 @@ import { generateClient } from "aws-amplify/data";
 import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 import { useQuery } from "@tanstack/react-query";
+import { FormEvent } from "react";
 
 const client = generateClient<Schema>();
 
 function App() {
-  let { isPending, error, data } = useQuery({
+  let posts = useQuery({
     queryKey: ["posts"],
     queryFn: () => client.models.Post.list(),
   });
+
+  async function create(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    await client.models.Post.create({
+      content: e.currentTarget.postContent.value,
+      title: e.currentTarget.postTitle.value,
+    });
+    posts.refetch();
+  }
 
   return (
     <Authenticator>
@@ -18,16 +28,24 @@ function App() {
         <main>
           <h1>My APP - {user?.signInDetails?.loginId ?? "Anonymous"}</h1>
 
-          {isPending
+          {posts.isLoading
             ? "loading"
-            : data
-              ? data.data.map((post) => (
+            : posts.data
+              ? posts.data.data.map((post) => (
                 <div>
                   <h1>{post.title}</h1>
                   <span>{post.content}</span>
                 </div>
               ))
-              : `error: ${error}`}
+              : `error: ${posts.error}`}
+
+          <form onSubmit={create}>
+            <h2>Title</h2>
+            <input type="text" name="postTitle"></input>
+            <h2>Content</h2>
+            <input type="text" name="postContent"></input>
+            <button type="submit">Create</button>
+          </form>
 
           <button onClick={signOut}>Sign out</button>
         </main>
