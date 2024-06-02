3a5bf4a48e4397d59a03d82bf99ab3543b9c043a
Alan Sartorio <alan42ga@hotmail.com>
2024-06-01 21:05:26 -0300

    Add posts list

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,35 @@
-//import { useEffect, useState } from "react";
-//import type { Schema } from "../amplify/data/resource";
-//import { generateClient } from "aws-amplify/data";
-import { Authenticator } from '@aws-amplify/ui-react'
-import '@aws-amplify/ui-react/styles.css'
+import { useEffect, useState } from "react";
+import type { Schema } from "../amplify/data/resource";
+import { generateClient } from "aws-amplify/data";
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import { useQuery } from "@tanstack/react-query";
 
-//const client = generateClient<Schema>();
+const client = generateClient<Schema>();
 
 function App() {
+  let { isPending, error, data } = useQuery({
+    queryKey: ["posts"],
+    queryFn: () => client.models.Post.list(),
+  });
+
   return (
     <Authenticator>
-      {({ signOut, user}) => (
+      {({ signOut, user }) => (
         <main>
           <h1>My APP - {user?.signInDetails?.loginId ?? "Anonymous"}</h1>
+
+          {isPending
+            ? "loading"
+            : data
+              ? data.data.map((post) => (
+                <div>
+                  <h1>{post.title}</h1>
+                  <span>{post.content}</span>
+                </div>
+              ))
+              : `error: ${error}`}
+
           <button onClick={signOut}>Sign out</button>
         </main>
       )}
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 Amplify.configure(outputs);
 const existingConfig = Amplify.getConfig();
@@ -14,8 +15,12 @@ Amplify.configure({
   },
 });
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
